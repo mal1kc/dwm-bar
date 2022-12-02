c2541f3b8955b5018559b4e2ac9c57a6975dae38
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-12-02 14:15:20 +0300

    updated pipewrie func

--- bar-functions/dwm_pipewire.sh
+++ bar-functions/dwm_pipewire.sh
@@ -1,29 +1,32 @@
 #!/bin/env sh
 
-
 # Dependencies: wireplumber
 
-dwm_pipewire () {
-    VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "[0-9].[0-9]+" )
-    STATE=$( wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "MUTED")
+dwm_pipewire() {
+	#VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "[0-9].[0-9]+" )
+	VOL=$(printf "%d" $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "[0-9]"))
+	STATE=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "MUTED")
 
-    printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$STATE" = "MUTED" ] || [ $(echo "$VOL == 0.0 " | bc -l) -eq 1 ]; then
-            printf "🔇"
-        elif [ $( echo "$VOL > 0 && $VOL < .33" | bc -l ) -eq 1 ]; then
-            printf "🔈 %s%%" "$VOL"
-        elif [ $(echo "$VOL > .33 && $VOL < .66 "| bc -l) -eq 1 ]; then
-            printf "🔉 %s%%" "$VOL"
-        else
-            printf "🔊 %s%%" "$VOL"
-        fi
-    else
-        if [ "$STATE" = "MUTED" ] || [ $(echo "$VOL == 0.0 " | bc -l) -eq 1 ]; then
-            printf "MUTED"
-        else
-            printf "VOL %s%%" "$VOL"
-        fi
-    fi
-    printf "%s\n" "$SEP2"
+	printf "%s" "$SEP1"
+	if [ "$IDENTIFIER" = "unicode" ]; then
+		if [ "$STATE" = "MUTED" ] || [ $VOL -eq 0 ]; then
+			printf "🔇"
+		else
+			VOL=${VOL##+(0)}
+			if [ $VOL -gt 0 ] && [ 33 -gt $VOL ]; then
+				printf "🔈 %s%%" "$VOL"
+			elif [ $VOL -gt 33 ] && [ 66 -gt $VOL ]; then
+				printf "🔉 %s%%" "$VOL"
+			else
+				printf "🔊 %s%%" "$VOL"
+			fi
+		fi
+	else
+		if [ "$STATE" = "MUTED" ] || [ $(echo "$VOL == 0.0 " | bc -l) -eq 1 ]; then
+			printf "MUTED"
+		else
+			printf "VOL %s%%" "$VOL"
+		fi
+	fi
+	printf "%s\n" "$SEP2"
 }
--- dwm_bar.sh
+++ dwm_bar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/dash
 
 # A modular status bar for dwm
 # Joe Standring <git@joestandring.com>
